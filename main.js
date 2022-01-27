f0092c421fd418e48f97e9906f573ec1601e308b
Serg <67319212+Dioweet-hash@users.noreply.github.com>
2022-01-27 10:08:57 +0200

    hah

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
+let numbers = ["GG"];
+
+
+function verticalChecker(randNumber) {
+
+}
+
+function horizontalChecker(randNumber) {
+  return(True)
+}
+
+function blockChecker(randNumber) {
+  return(True)
+}
+
 function numberGenerator() {
-  let i = 1
-  while(i<82){
-    let randNumber = Math.floor(Math.random() * 9);
+  let i = 1;
+  while(i<=81){
+    let randNumber = Math.floor(Math.random() * 9)+1;
     document.getElementById(i).value = randNumber;
+    verticalChecker(randNumber)
+    if()
+    numbers.push(randNumber);
     i++;
   }
+  //alert(numbers.length)
+  console.log(numbers);
 }
 
 numberGenerator()
